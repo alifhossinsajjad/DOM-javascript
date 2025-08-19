2fc2e1a2cc4613274d3d430a8676cd56ca17bc0c
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-19 21:54:23 +0600

    3 way of dom event hadleing

--- js/events.js
+++ js/events.js
@@ -1,1 +1,21 @@
-console.log('abar chole aslam')
+
+/**  option 2 for event handler  */
+
+
+
+function nowMyTurn() {
+  document.body.style.backgroundColor = "red";
+}
+
+function youCan() {
+  document.body.style.fontSize = "30px";
+}
+
+
+// option 3 : get Element by id and then set onclick
+   const makeIt = document.getElementById('make-it')
+
+            makeIt.onclick = function makeIt(){
+                document.body.style.backgroundColor = 'green'
+            }
+            
