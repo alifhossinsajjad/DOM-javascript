974cc103be882c57db59ca2a48a6a3d4e24fb17d
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-08-19 23:40:24 +0600

    how to use addEventlistener

--- js/events.js
+++ js/events.js
@@ -1,7 +1,4 @@
-
 /**  option 2 for event handler  */
-
-
 
 function nowMyTurn() {
   document.body.style.backgroundColor = "red";
@@ -11,11 +8,23 @@ function youCan() {
   document.body.style.fontSize = "30px";
 }
 
-
 // option 3 : get Element by id and then set onclick
-   const makeIt = document.getElementById('make-it')
+const makeIt = document.getElementById("make-it");
 
-            makeIt.onclick = function makeIt(){
-                document.body.style.backgroundColor = 'green'
-            }
-            
+makeIt.onclick = function makeIt() {
+  document.body.style.backgroundColor = "green";
+};
+
+// option 4 :
+
+document
+  .getElementById("network-engineer")
+  .addEventListener("click", function engineer() {
+    document.body.style.backgroundColor = "purple";
+  });
+
+document
+  .getElementById("software-engineer")
+  .addEventListener("click", function software() {
+    document.body.style.fontSize = "40px";
+  });
